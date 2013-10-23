4e9a551822633bf386b57107ed565266fe5bb976
mile <mile@Mile-Laptop>
2013-10-23 20:40:11 +0200

    Stack view item

--- protected/views/stack/view.php
+++ protected/views/stack/view.php
@@ -16,7 +16,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Stack #<?php echo $model->id; ?></h1>
 <table id="yw0" class="detail-view">
     <tbody>
         <tr class="odd"><th><?php echo $model->getAttributeLabel('id'); ?></th><td><?php echo $model->id; ?></td></tr>
@@ -30,61 +29,7 @@ $this->menu=array(
         <tr class="even"><th><?php echo $model->getAttributeLabel('date_created'); ?></th><td><?php echo $model->date_created; ?></td></tr>
         <tr class="even">
             <th><?php echo $model->getAttributeLabel('data'); ?></th>
-            <td><?php echo CHtml::textArea('data', indent($model->data), array('class'=>'coefficients-textarea','readonly'=>'readonly')); ?></td>
+            <td><?php echo CHtml::textArea('data', Oddsleaders::pretty_print($model->data), array('class'=>'coefficients-textarea','readonly'=>'readonly')); ?></td>
         </tr>
     </tbody>
 </table>
-
-<?php
-function indent($json) {
-
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
-
-    for ($i=0; $i<=$strLen; $i++) {
-
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
-
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-
-        // If this character is the end of an element,
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        // Add the character to the result string.
-        $result .= $char;
-
-        // If the last character was the beginning of an element,
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        $prevChar = $char;
-    }
-
-    return $result;
-}
-
-?>
