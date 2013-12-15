43317858fff82db835c6345adfdb3ccc6bcfd09e
mile <mile@Mile-Laptop>
2013-12-15 11:13:38 +0100

    New Db table created

--- protected/views/admin/admin_menu.php
+++ protected/views/admin/admin_menu.php
@@ -6,6 +6,7 @@
     <li><?php echo CHtml::link("Names Admin", Yii::app()->createUrl("names/admin")); ?></li>
     <li>-------------------------------------------------------------------------------------</li>
     <li><?php echo CHtml::link("Stack Old admin", Yii::app()->createUrl("stackOld/admin")); ?></li>
+    <li><?php echo CHtml::link("Stack Archive admin", Yii::app()->createUrl("archive/admin")); ?></li>
     <li>-------------------------------------------------------------------------------------</li>
     <li><?php echo CHtml::link("User admin", Yii::app()->createUrl("user/admin")); ?></li>
     <li><?php echo CHtml::link("Role admin", Yii::app()->createUrl("role/admin")); ?></li>
